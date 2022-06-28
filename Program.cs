6f893a0c90e56766d60bcd52b7eff02af2dacb2a
Çaglar <caglar_G@live.nl>
2022-06-28 14:50:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+string[] ListOfStacks= Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+
+foreach (var stack in ListOfStacks)
+{
+    Console.WriteLine("The application will now initialise the following stacks: " + stack);
+
+}
+
+/*foreach (var stacks in ListOfStacks)
+{
+    Process.Start("cmd.exe", string.Format("/C Terraform.exe -chdir={0} init", stacks));
+    Console.WriteLine(string.Format("initialising folder: {0}", stacks));
+}*/
+Console.Read();
